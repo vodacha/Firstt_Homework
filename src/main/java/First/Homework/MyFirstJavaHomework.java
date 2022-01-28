6c556de5889d4cf51e0e3f2d115363918e19a8bb
Andrey Kolev <andrei_kolev1@abv.bg>
2022-01-28 19:32:15 +0200

    My First Java Homework

--- src/main/java/First/Homework/MyFirstJavaHomework.java
+++ src/main/java/First/Homework/MyFirstJavaHomework.java
@@ -1,6 +1,24 @@
 package First.Homework;
 
+import java.util.Scanner;
+
 public class MyFirstJavaHomework {
 
-    
-}
+    public static void main (String[] args) {
+
+        System.out.println("Enter your name:");
+
+        Scanner scanner = new Scanner(System.in);
+
+        String firstName = scanner.next();
+
+        System.out.println("Enter your favourite hobby: ");
+        String Hobby = scanner.next();
+
+        System.out.println("Hello " + firstName + " your favourite hobby is: " + Hobby);
+
+
+    }
+    }
+
+
